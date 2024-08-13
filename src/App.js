51776785b1d85093db083c57a5c9857ff7c313f3
Palacharlabhargavi <71444458+Palacharlabhargavi@users.noreply.github.com>
2024-08-13 06:52:35 +0000

    DAY2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import './App.css';
-import {Routes, Route} from "react-router-dom";
-//React slick
+import "./App.css";
+
+//routers
+import { Routes, Route } from "react-router-dom";
+
+//React slick(copy paste in this website)
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+// Pages
+import PlayPage from './pages/Play.page';
+import MoviePage from './pages/Movie.page';
+import HomePage from './pages/Home.page';
+
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" />
-      <Route path="/movie/:id"  />
-      <Route path="/plays"  />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MoviePage />} />
+      <Route path="/plays" element={<PlayPage />} />
     </Routes>
-    
   );
 }
 
