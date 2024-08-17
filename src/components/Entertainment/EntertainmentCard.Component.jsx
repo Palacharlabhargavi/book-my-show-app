00e866f51ea6beb22b29b407b52d7b7de3b4a868
Palacharlabhargavi <71444458+Palacharlabhargavi@users.noreply.github.com>
2024-08-17 06:32:18 +0000

    updated DAY3

--- src/components/Entertainment/EntertainmentCard.Component.jsx
+++ src/components/Entertainment/EntertainmentCard.Component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const EntertainmentCard = (props) => {
   return (
@@ -17,18 +19,17 @@ const EntertainmentCard = (props) => {
 
 const EntertainmentCardSlider = () => {
   const EntertainmentImage = [
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTI1KyBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/workshops-collection-202007231330.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-OSBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/fitness-collection-2020081150.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MjArIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/kids-collection-202007220710.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-NDArIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/comedy-shows-collection-202007220710.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-NSBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/music-shows-collection-202007220710.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTUrIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/esports-collection-202011150107.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-OSBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/self-improvement-collection-202007220710.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:w-300/cooking-collection-202007222211.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MSBFdmVudA%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/interactive-games-collection-202012041129.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTArIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/art-and-crafts-collection-202007220710.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-NCBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/theatre-shows-collection-202012041128.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:w-300/adventure-collection-202010140844.png",
+    "https://cdn4.vectorstock.com/i/1000x1000/04/33/workshop-icon-vector-6280433.jpg",
+    "https://tse2.mm.bing.net/th?id=OIP.6c0GT8ciPGro5Mwnadz93gHaH_&pid=Api&P=0&h=180",
+    "https://tse4.mm.bing.net/th?id=OIP.Sv5oohjOZ-DsbbpGG66oJQHaH5&pid=Api&P=0&h=180",
+    "https://cdn4.vectorstock.com/i/1000x1000/65/33/comedy-show-logo-badge-emblem-design-with-old-vector-19436533.jpg",
+    "https://tse3.mm.bing.net/th?id=OIP.fW-HFQa2RsMqbwg2TBu5jwHaH_&pid=Api&P=0&h=180",
+    "https://cdn1.vectorstock.com/i/1000x1000/75/60/logo-sport-shop-vector-5857560.jpgg",
+    "https://tse3.mm.bing.net/th?id=OIP.T6m08SRRuFiFPCp2MrGsvwHaH6&pid=Api&P=0&h=180",
+    "https://tse4.mm.bing.net/th?id=OIP.bI5yXZnSmk7HMgaIdzfHEgHaH_&pid=Api&P=0&h=180",
+    "https://tse3.mm.bing.net/th?id=OIP.tgbCLJNiHmlzJuwcI3u1hgHaH_&pid=Api&P=0&h=180",
+    "https://i.pinimg.com/originals/82/3b/86/823b86aca4b4b40bca8675750b1986be.jpg",
+    "https://tse1.mm.bing.net/th?id=OIP.4DHx1vAyobRLB_CQ-WjyeQHaH_&pid=Api&P=0&h=180",
   ];
 
   const settings = {
@@ -40,15 +41,15 @@ const EntertainmentCardSlider = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2,
+          slidesToShow: 4,
+          slidesToScroll: 3,
         },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1,
+          slidesToScroll: 2,
           initialSlide: 1,
         },
       },
